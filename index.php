9ab25750550434c6ed47a27a62b38fadb5f6607c
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-09 09:31:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $month = array('janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre');
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>partie 5 exercice 4</title>
+    </head>
+    <body>
+        <p> <?php  $month[7] = 'août' ?>
+        </p>
+    </body>
+</html>
